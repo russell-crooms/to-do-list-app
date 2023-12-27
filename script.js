825f59684834f5ed941dcb8b4bbc762a88c623d9
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2023-12-27 08:36:53 -0600

    Add functionality to our list items

--- script.js
+++ script.js
@@ -13,5 +13,16 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = ''; 
-    
 }
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked"); 
+    } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove(); 
+        }
+    }, false);
+
+
+
+
